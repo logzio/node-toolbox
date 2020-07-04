a50f0f967bcbd6d8cfbcb471633a93e2cb4b786e
madvinking <nir.winkler@logz.io>
2020-07-04 19:09:03 +0300

    Export logger

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const logger = require('./logger');
-const config = require('./config');
-const utils = require('./utils');
-const metrics = require('./metrics');
-const trace = require('./trace');
-const middlewares = require('./middlewares');
-
-module.exports = {
-  middlewares,
-  ...logger,
-  ...config,
-  ...metrics,
-  ...trace,
-  ...utils,
-};
+export * as logger from './packages/logger/src/index.js';
