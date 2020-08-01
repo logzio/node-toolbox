0c953f2fadd5f153339e0ce2ee015e15a3ef6bd1
madvinking <nir.winkler@logz.io>
2020-08-01 16:49:53 +0300

    Export all from main

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export * as logger from './packages/logger/src/index.js';
+export * from './packages/config/src/index.js';
+export * from './packages/consul/src/index.js';
+export * from './packages/logger/src/index.js';
+export * from './packages/metrics/src/index.js';
+export * from './packages/server/src/index.js';
+export * from './packages/tracer/src/index.js';
+export * from './packages/tracer-opentelemtry/src/index.js';
+export * from './packages/utils/src/index.js';
